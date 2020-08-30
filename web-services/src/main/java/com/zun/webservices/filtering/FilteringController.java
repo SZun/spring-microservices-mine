1b1429d2e7efa0b11fcc601301586f850aaa8f7f
Zun <zunsamuel97@gmail.com>
2020-08-30 00:21:28 -0500

    step 25

--- web-services/src/main/java/com/zun/webservices/filtering/FilteringController.java
+++ web-services/src/main/java/com/zun/webservices/filtering/FilteringController.java
@@ -1,5 +1,9 @@
 package com.zun.webservices.filtering;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,18 +14,30 @@ import java.util.List;
 public class FilteringController {
 
     @GetMapping(path = "/filtering")
-    public SomeBean retrieveSomeBean(){
-        return new SomeBean("value1","value2","value3");
+    public MappingJacksonValue retrieveSomeBean(){
+        SomeBean someBean = new SomeBean("value1", "value2", "value3");
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1, field2");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        MappingJacksonValue mapper = new MappingJacksonValue(someBean);
+        mapper.setFilters(filters);
+        return mapper;
     }
 
     @GetMapping(path="/filtering-list")
-    public List<SomeBean> retrieveLostOfSomeBean(){
-        return Arrays.asList(
-                new SomeBean("value1","value2","value3"),
-                new SomeBean("value1","value2","value3"),
-                new SomeBean("value1","value2","value3"),
-                new SomeBean("value1","value2","value3")
+    public MappingJacksonValue retrieveLostOfSomeBean(){
+        List<SomeBean> someBeans = Arrays.asList(
+                new SomeBean("value1", "value2", "value3"),
+                new SomeBean("value1", "value2", "value3"),
+                new SomeBean("value1", "value2", "value3"),
+                new SomeBean("value1", "value2", "value3")
         );
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field2, field3");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        MappingJacksonValue mapper = new MappingJacksonValue(someBeans);
+        mapper.setFilters(filters);
+
+        return mapper;
     }
 
 }
