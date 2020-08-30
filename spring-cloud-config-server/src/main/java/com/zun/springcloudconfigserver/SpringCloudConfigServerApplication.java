65d22b5263e85dbf8298afa965c326f1237a36d8
Zun <zunsamuel97@gmail.com>
2020-08-30 04:43:39 -0500

    step 07

--- spring-cloud-config-server/src/main/java/com/zun/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/zun/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -11,7 +11,7 @@ public class SpringCloudConfigServerApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
-		
+
 	}
 
 }
