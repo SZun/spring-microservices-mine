d0cb2c47f6b705d3e5fb3a1e82cf67002d2efade
Zun <zunsamuel97@gmail.com>
2020-09-03 11:38:22 -0500

    step 7

--- web-services/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into user(id, name, birth_date) values(10001, 'AB', sysdate());
-insert into user(id, name, birth_date) values(10002, 'Jack', sysdate());
-insert into user(id, name, birth_date) values(10003, 'Jill', sysdate());
-
-insert into post(id, description, user_id) values(11001, 'My First Post', 10001)
-insert into post(id, description, user_id) values(11002, 'My Second Post', 10001)
